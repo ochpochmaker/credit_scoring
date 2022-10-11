981d3ac6c6ff0b33ed911ccaa223692cd1b0e6cf
ochpochmaker <salakhovmg.olymp@gmail.com>
2022-10-12 02:17:36 +0300

    Done(maybe)

--- findata-videogames.py
+++ findata-videogames.py
@@ -1,9 +1,9 @@
 import pandas as pd
+from sklearn.metrics import mean_absolute_error
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_classification
 from sklearn import datasets, ensemble
 from sklearn.preprocessing import OneHotEncoder
 import sklearn as sk
@@ -14,80 +14,139 @@ from sklearn.metrics import confusion_matrix
 from sklearn.svm import LinearSVC
 from sklearn.preprocessing import LabelEncoder
 
-Video_Games = pd.read_csv(r'C:\Users\Админ\Desktop\Папки\findata-videogames\Video_Games.csv')
+Video_Games_train = pd.read_csv(r'C:\Users\Админ\Desktop\Папки\findata-videogames\Video_Games.csv')
+Video_Games_test = pd.read_csv(r'C:\Users\Админ\Desktop\Папки\findata-videogames\Video_Games_Test.csv')
+
 
 #категорриальные признаки заменяем  на моду
-Video_Games['Genre'].fillna(Video_Games['Genre'].mode(), inplace=True)
-Video_Games['Rating'].fillna(Video_Games['Rating'].mode(), inplace=True)
+Video_Games_train['Genre'].fillna(Video_Games_train['Genre'].mode(), inplace=True)
+Video_Games_train['Rating'].fillna(Video_Games_train['Rating'].mode(), inplace=True)
+
+Video_Games_test['Genre'].fillna(Video_Games_test['Genre'].mode(), inplace=True)
+Video_Games_test['Rating'].fillna(Video_Games_test['Rating'].mode(), inplace=True)
+
+
 #числовые признаки заменяем  на медиану
-Video_Games['NA_Sales'].fillna(Video_Games['NA_Sales'].median(), inplace=True)
-Video_Games['EU_Sales'].fillna(Video_Games['EU_Sales'].median(), inplace=True)
-Video_Games['Other_Sales'].fillna(Video_Games['Other_Sales'].median(), inplace=True)
+Video_Games_train['NA_Sales'].fillna(Video_Games_train['NA_Sales'].median(), inplace=True)
+Video_Games_train['EU_Sales'].fillna(Video_Games_train['EU_Sales'].median(), inplace=True)
+Video_Games_train['Other_Sales'].fillna(Video_Games_train['Other_Sales'].median(), inplace=True)
+
+Video_Games_test['NA_Sales'].fillna(Video_Games_test['NA_Sales'].median(), inplace=True)
+Video_Games_test['EU_Sales'].fillna(Video_Games_test['EU_Sales'].median(), inplace=True)
+Video_Games_test['Other_Sales'].fillna(Video_Games_test['Other_Sales'].median(), inplace=True)
+
+
 # остальное заменяем на нули
-Video_Games['Publisher'].fillna(-1000)
-Video_Games['Developer'].fillna(-1000)
-Video_Games['Critic_Score'].fillna(-1000)
-Video_Games['Critic_Count'].fillna(-1000)
-Video_Games['User_Score'].fillna(-1000)
-Video_Games['User_Count'].fillna(-1000)
+Video_Games_train['Year_of_Release'].fillna(Video_Games_train['Year_of_Release'].median(), inplace=True)
+Video_Games_train['Publisher'].fillna('0', inplace=True)
+Video_Games_train['Developer'].fillna('0', inplace=True)
+Video_Games_train['Critic_Score'].fillna(0, inplace=True)
+Video_Games_train['Critic_Count'].fillna(0, inplace=True)
+Video_Games_train['User_Score'].fillna(0, inplace=True)
+Video_Games_train['User_Count'].fillna(0, inplace=True)
+
+Video_Games_test['Year_of_Release'].fillna(Video_Games_test['Year_of_Release'].median(), inplace=True)
+Video_Games_test['Publisher'].fillna('0', inplace=True)
+Video_Games_test['Developer'].fillna('0', inplace=True)
+Video_Games_test['Critic_Score'].fillna(0, inplace=True)
+Video_Games_test['Critic_Count'].fillna(0, inplace=True)
+Video_Games_test['User_Score'].fillna(0, inplace=True)
+Video_Games_test['User_Count'].fillna(0, inplace=True)
+
 
 #добавляем хуеву гору новых флагов
 ohe = OneHotEncoder(sparse=False)
 
-Genre_flg = ohe.fit_transform(Video_Games['Genre'].values.reshape(-1, 1))
+Genre_flg = ohe.fit_transform(Video_Games_train['Genre'].values.reshape(-1, 1))
 tmp = pd.DataFrame(Genre_flg, columns=['Genre=' + str(i) for i in range(Genre_flg.shape[1])])
-Video_Games = pd.concat([Video_Games, tmp], axis=1)
+Video_Games_train = pd.concat([Video_Games_train, tmp], axis=1)
 
-Platform_flg = ohe.fit_transform(Video_Games['Platform'].values.reshape(-1, 1))
+Platform_flg = ohe.fit_transform(Video_Games_train['Platform'].values.reshape(-1, 1))
 tmp = pd.DataFrame(Platform_flg, columns=['Platform=' + str(i) for i in range(Platform_flg.shape[1])])
-Video_Games = pd.concat([Video_Games, tmp], axis=1)
+Video_Games_train = pd.concat([Video_Games_train, tmp], axis=1)
 
-Publisher_flg = ohe.fit_transform(Video_Games['Publisher'].values.reshape(-1, 1))
+Publisher_flg = ohe.fit_transform(Video_Games_train['Publisher'].values.reshape(-1, 1))
 tmp = pd.DataFrame(Publisher_flg, columns=['Publisher=' + str(i) for i in range(Publisher_flg.shape[1])])
-Video_Games = pd.concat([Video_Games, tmp], axis=1)
+Video_Games_train = pd.concat([Video_Games_train, tmp], axis=1)
 
-Developer_flg = ohe.fit_transform(Video_Games['Developer'].values.reshape(-1, 1))
+Developer_flg = ohe.fit_transform(Video_Games_train['Developer'].values.reshape(-1, 1))
 tmp = pd.DataFrame(Developer_flg, columns=['Developer=' + str(i) for i in range(Developer_flg.shape[1])])
-Video_Games = pd.concat([Video_Games, tmp], axis=1)
+Video_Games_train = pd.concat([Video_Games_train, tmp], axis=1)
 
-Rating_flg = ohe.fit_transform(Video_Games['Rating'].values.reshape(-1, 1))
+Rating_flg = ohe.fit_transform(Video_Games_train['Rating'].values.reshape(-1, 1))
 tmp = pd.DataFrame(Rating_flg, columns=['Rating=' + str(i) for i in range(Rating_flg.shape[1])])
-Video_Games = pd.concat([Video_Games, tmp], axis=1)
+Video_Games_train = pd.concat([Video_Games_train, tmp], axis=1)
 
-#причесываем числовые признаки
-le = LabelEncoder()
-le.fit(Video_Games.Year_of_Release)
-Video_Games['Year_of_Release_le'] = le.transform(Video_Games.Year_of_Release)
-le.fit(Video_Games.NA_Sales)
-Video_Games['NA_Sales_le'] = le.transform(Video_Games.NA_Sales)
-le.fit(Video_Games.EU_Sales)
-Video_Games['EU_Sales_le'] = le.transform(Video_Games.EU_Sales)
-le.fit(Video_Games.Other_Sales)
-Video_Games['Other_Sales_le'] = le.transform(Video_Games.Other_Sales)
-le.fit(Video_Games.Critic_Score)
-Video_Games['Critic_Score_le'] = le.transform(Video_Games.Critic_Score)
-le.fit(Video_Games.Critic_Count)
-Video_Games['Critic_Count_le'] = le.transform(Video_Games.Critic_Count)
-le.fit(Video_Games.User_Score)
-Video_Games['User_Score_le'] = le.transform(Video_Games.User_Score)
-le.fit(Video_Games.User_Count)
-Video_Games['User_Count_le'] = le.transform(Video_Games.User_Count)
+
+Genre_flg = ohe.fit_transform(Video_Games_test['Genre'].values.reshape(-1, 1))
+tmp = pd.DataFrame(Genre_flg, columns=['Genre=' + str(i) for i in range(Genre_flg.shape[1])])
+Video_Games_test = pd.concat([Video_Games_test, tmp], axis=1)
+
+Platform_flg = ohe.fit_transform(Video_Games_test['Platform'].values.reshape(-1, 1))
+tmp = pd.DataFrame(Platform_flg, columns=['Platform=' + str(i) for i in range(Platform_flg.shape[1])])
+Video_Games_test = pd.concat([Video_Games_test, tmp], axis=1)
+
+Publisher_flg = ohe.fit_transform(Video_Games_test['Publisher'].values.reshape(-1, 1))
+tmp = pd.DataFrame(Publisher_flg, columns=['Publisher=' + str(i) for i in range(Publisher_flg.shape[1])])
+Video_Games_test = pd.concat([Video_Games_test, tmp], axis=1)
+
+Developer_flg = ohe.fit_transform(Video_Games_test['Developer'].values.reshape(-1, 1))
+tmp = pd.DataFrame(Developer_flg, columns=['Developer=' + str(i) for i in range(Developer_flg.shape[1])])
+Video_Games_test = pd.concat([Video_Games_test, tmp], axis=1)
+
+Rating_flg = ohe.fit_transform(Video_Games_test['Rating'].values.reshape(-1, 1))
+tmp = pd.DataFrame(Rating_flg, columns=['Rating=' + str(i) for i in range(Rating_flg.shape[1])])
+Video_Games_test = pd.concat([Video_Games_test, tmp], axis=1)
+
+
+# #причесываем числовые признаки
+# le = LabelEncoder()
+# le.fit(Video_Games_train.Year_of_Release)
+# Video_Games_train['Year_of_Release_le'] = le.transform(Video_Games_train.Year_of_Release)
+
 
 #убираем ненужные признаки
-Video_Games = Video_Games.drop(('Name'), axis=1)
-Video_Games = Video_Games.drop(('Platform'), axis=1)
-Video_Games = Video_Games.drop(('Genre'), axis=1)
-Video_Games = Video_Games.drop(('Publisher'), axis=1)
-Video_Games = Video_Games.drop(('Developer'), axis=1)
-Video_Games = Video_Games.drop(('Rating'), axis=1)
+Video_Games_train = Video_Games_train.drop(('Name'), axis=1)
+Video_Games_train = Video_Games_train.drop(('Platform'), axis=1)
+Video_Games_train = Video_Games_train.drop(('Genre'), axis=1)
+Video_Games_train = Video_Games_train.drop(('Publisher'), axis=1)
+Video_Games_train = Video_Games_train.drop(('Developer'), axis=1)
+Video_Games_train = Video_Games_train.drop(('Rating'), axis=1)
+
+Video_Games_test = Video_Games_test.drop(('Name'), axis=1)
+Video_Games_test = Video_Games_test.drop(('Platform'), axis=1)
+Video_Games_test = Video_Games_test.drop(('Genre'), axis=1)
+Video_Games_test = Video_Games_test.drop(('Publisher'), axis=1)
+Video_Games_test = Video_Games_test.drop(('Developer'), axis=1)
+Video_Games_test = Video_Games_test.drop(('Rating'), axis=1)
 
 
+#поиск нулов
+val = Video_Games_train.isna().any()[lambda x: x]
+print(val)
+val2 = Video_Games_test.isna().any()[lambda x: x]
+print(val2)
 
 #делим датасет на параметры и таргеты
-X = Video_Games.drop(('JP_Sales'), axis=1)
-y = Video_Games['JP_Sales']
-#кросс-валидация
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=33)
+X_train = Video_Games_train.drop(('JP_Sales'), axis=1)
+Y_train = Video_Games_train['JP_Sales']
+X_test = Video_Games_test.drop(('Id'), axis=1)
+Test_Id = Video_Games_test['Id']
+print('X_train',X_train.shape)
+print('Y_train',Y_train.shape)
+print('X_test',X_test.shape)
+print('Test_Id',X_train.shape)
+# #кросс-валидация
+X_train1, X_test1, Y_train1, Y_test1 = train_test_split(X_train, Y_train, test_size=0.33, random_state=33)
+print('X_train',X_train.shape)
+print('Y_train',Y_train.shape)
+print('X_test',X_test.shape)
+print('Test_Id',X_train.shape)
+
+print('X_train1',X_train.shape)
+print('Y_train1',Y_train.shape)
+print('X_test1',X_test.shape)
+print('Y_test1',X_train.shape)
 
 #используем GradientBoostingRegressor, потому что препод использовал GradientBoostingRegressor
 params = {
@@ -98,13 +157,15 @@ params = {
     "loss": "squared_error",
 }
 
-X_train, Y_train = make_classification(n_samples=len(X_train))
+print(X_train.shape)
 gbr = ensemble.GradientBoostingRegressor(**params)
-gbr.fit(X_train, y_train)
+gbr.fit(X_train1, Y_train1)
+Y_test_gbr = gbr.predict(X_test1)
+error = mean_absolute_error(Y_test1, Y_test_gbr)
+print(error)
 
-
+gbr.fit(X_train, Y_train)
 Y_test_gbr = gbr.predict(X_test)
-X_test_gbr = X_test[:, 0]
 
-Video_Games_Itog = pd.DataFrame({"Id": X_test_gbr, "JP_Sales": Y_test_gbr})
-print(Video_Games_Itog)
+itog = pd.DataFrame({"Id": Test_Id,"JP_Sales": Y_test_gbr})
+itog.to_csv("itog2.csv", index=False)
--- findata_videogames_2.py
+++ findata_videogames_2.py
@@ -3,7 +3,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 Video_Games = pd.read_csv(r'C:\Users\Админ\Desktop\Папки\findata-videogames\Video_Games.csv')
 
-ohe = OneHotEncoder(sparse=False)
+# ohe = OneHotEncoder(sparse=False)
 #
 # Genre_flg = ohe.fit_transform(Video_Games['Genre'].values.reshape(-1, 1))
 # tmp = pd.DataFrame(Genre_flg, columns=['Genre=' + str(i) for i in range(Genre_flg.shape[1])])
@@ -22,9 +22,9 @@ ohe = OneHotEncoder(sparse=False)
 # tmp = pd.DataFrame(Developer_flg, columns=['Developer=' + str(i) for i in range(Developer_flg.shape[1])])
 # Video_Games = pd.concat([Video_Games, tmp], axis=1)
 #
-Rating_flg = ohe.fit_transform(Video_Games['Rating'].values.reshape(-1, 1))
-tmp = pd.DataFrame(Rating_flg, columns=['Rating=' + str(i) for i in range(Rating_flg.shape[1])])
-Video_Games = pd.concat([Video_Games, tmp], axis=1)
+# Rating_flg = ohe.fit_transform(Video_Games['Rating'].values.reshape(-1, 1))
+# tmp = pd.DataFrame(Rating_flg, columns=['Rating=' + str(i) for i in range(Rating_flg.shape[1])])
+# Video_Games = pd.concat([Video_Games, tmp], axis=1)
 #
 #
 # print(Video_Games)
@@ -46,12 +46,16 @@ Video_Games = pd.concat([Video_Games, tmp], axis=1)
 #
 # # #анализ датасета
 # # print(Video_Games.head())#Что есть наши данные
-# # print(Video_Games.shape) # мы увидим информацию о размерности нашего датафрейма
-# # print(Video_Games.info()) # покажет информацию о размерности данных
+print(Video_Games.shape) # мы увидим информацию о размерности нашего датафрейма
+print(Video_Games.info()) # покажет информацию о размерности данных
 # # print(Video_Games.describe()) # показывает статистики count,mean, std, min, 25%-50%-75% percentile, max
 # # print(Video_Games.nunique()) # количество уникальных значений для каждого столбца
 # # #Также было бы неплохо увидеть информацию о количестве каждого уникального значения для каждого столбца в наборе данных
-feature_names = Video_Games.columns.tolist()
-for column in feature_names:
-    print(column)
-    print(Video_Games[column].value_counts(dropna=False))
+# feature_names = Video_Games.columns.tolist()
+# for column in feature_names:
+#     print(column)
+#     print(Video_Games[column].value_counts(dropna=False))
+#
+# #поиск нулов
+# val = Video_Games.isna().any()[lambda x: x]
+# print(val)
